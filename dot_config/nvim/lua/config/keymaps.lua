a735f5c7d40b41ddcb7f9ae3c5fa8cc40a0682d0
Reuben Reyes <reuben.a.reyes@gmail.com>
2023-08-29 00:07:56 -0700

    lazyvim

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- prev, next buffer
+vim.keymap.set("n", "<C-p>", ":bp<CR>")
+vim.keymap.set("n", "<C-n>", ":bn<CR>")
+
+-- jk is esc
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("c", "jk", "<C-c")
---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.cmdheight = 2
+vim.opt.fixendofline = false
+vim.opt.foldenable = false
+vim.opt.swapfile = false
+
+-- theme
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_transpaarent = true

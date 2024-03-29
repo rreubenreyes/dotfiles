982c7a4f0ddb7b65b56bbea9557165f88b5512c6
Reuben Reyes <reuben@humanapi.co>
2024-03-29 11:27:03 -0700

    commit hapi machine

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,6 @@ return {
       },
     },
     servers = {
-      terraformls = {},
       gopls = {
         keys = {
           -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12

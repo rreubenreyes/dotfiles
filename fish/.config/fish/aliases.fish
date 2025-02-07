d3576686fd540b3e9b42b65f31e1aefebf659305
Reuben Reyes <reuben.a.reyes@gmail.com>
2025-02-06 21:55:47 -0800

    helix config

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,15 @@
+# Aliases which shadow default bindings
+alias cat=bat
+alias vimdiff='nvim -d'
+alias pip='pip3'
+
+# Config files (cf*)
+alias cft='hx ~/.wezterm.lua'
+alias cfh='hx ~/.config/helix/config.toml'
+alias cff='hx ~/.config/fish/config.fish'
+
+# Convenience aliases
+alias finder='open -a "Finder"'
+alias l='eza --icons -laH'
+alias ls=eza
+alias z=zoxide
